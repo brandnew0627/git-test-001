e42ea6667c074b8fe82bdfb82354be407719e74b
bn_quan <825781891@qq.com>
2021-04-30 11:00:37 +0800

    second commit by IDEA

--- src/main/java/com/quan/git/HelloGit.java
+++ src/main/java/com/quan/git/HelloGit.java
@@ -3,5 +3,6 @@ package com.quan.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("version 2.0");
     }
 }
