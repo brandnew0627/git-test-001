651adb2003552f74322d598ec8902f733edbabae
brandnew0627 <71919320+brandnew0627@users.noreply.github.com>
2021-07-14 09:55:15 +0800

    Update HelloGit.java

--- src/main/java/com/quan/git/HelloGit.java
+++ src/main/java/com/quan/git/HelloGit.java
@@ -4,7 +4,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("version 2.0");
-        System.out.println("hello~QUAN~");
+        System.out.println("hello~QUAN~222");
         System.out.println("edit from github.");
         System.out.println("hello gitee!");
     }
